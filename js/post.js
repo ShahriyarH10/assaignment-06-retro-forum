2109c666b5e74f3df4cc9c9c98eef93db99fe44b
ShahriyarH10 <md.shahriyarhasan2002me@gmail.com>
2024-03-04 22:55:18 +0600

    lets discuss section and js file added

---
+++ js/post.js
@@ -0,0 +1,25 @@
+function loadPosts() {
+    fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data))
+}
+
+function displayPosts(posts){
+    console.log(posts);
+
+    const postsContainer = document.getElementById('posts-container');
+
+    for(const post of posts) {
+        const postDiv = document.createElement('div');
+        postDiv.innerHTML =`
+            <h4> ${post.id} </h4>
+            <h5> ${post.category} </h5>
+            <img src="${post.image}" alt="Post Image">
+            <p> ${post.isActive} </p>
+            <p> ${post.title} </p>
+        `;
+        postsContainer.appendChild(postDiv);
+    }
+}
+
+loadPosts();
